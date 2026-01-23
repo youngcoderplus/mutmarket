71ccfa3a398dcc6a396ef7e22b1fa1399ec11e6c
YoungCoder1 <victorywinter15@gmail.com>
2026-01-23 11:44:50 +0300

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -115,8 +115,15 @@ document.querySelectorAll('.js-add-to-cart')
   }); 
    }
 
+
+   let cartQuantity = 0;
+   cart.forEach((item) => {
+    cartQuantity += item.quantity;
+   });
    
-  console.log(cart)
+   document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+   //console.log(cartQuantity);
+   //console.log(cart)
   });
 });
 
