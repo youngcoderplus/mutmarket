44cdb142273798432c28cbe79f3015f7ee27fe12
YoungCoder1 <victorywinter15@gmail.com>
2026-02-02 10:44:01 +0300

    Make paymentSummary interactive

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -11,7 +11,7 @@ export function renderPaymentSummary(){
         const product = getProduct(cartItem.productId);
         productPriceCents += product.priceCents * cartItem.quantity;
 
-        const deliveryOption = getDeliveryOption(cartItem.getDeliveryOption);
+        const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
         shippingPriceCents += deliveryOption.priceCents;
     });
 
