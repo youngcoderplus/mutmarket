c78162594e2c6e3358966ba65ad7d89994910a3c
YoungCoder1 <victorywinter15@gmail.com>
2026-02-03 10:20:32 +0300

    Add jasmin testing framework

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -1,7 +1,6 @@
 import {formatCurrency} from "../scripts/utils/money.js";
 
 console.log('test suite: formatCurrency');
-
 console.log('converts cents into dollars');
 
 if (formatCurrency(2095) === '20.95') {
@@ -11,7 +10,6 @@ if (formatCurrency(2095) === '20.95') {
 }
 
 console.log('work with 0 ');
-
 
 if (formatCurrency(0) === '0.00') {
     console.log('passed');
